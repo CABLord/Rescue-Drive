ce5f79706096f09a9d1d1e8d88bd43bd059b9a66
Felix07OH <sthinfel@bx.fallmerayer.it>
2025-02-17 15:04:20 +0100

    Delete Control directory
    Hintoreggor isch gay

--- Control/V4.0/php/jscript/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("mobileMenu");
-    if (menu.classList.contains("w3-show")) {
-        menu.classList.remove("w3-show");
-    } else {
-        menu.classList.add("w3-show");
-    }
-}
