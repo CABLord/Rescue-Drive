8627ede80fe9fbb77b371756210bcf332ef69371
firefox0520 <136090179+firefox0520@users.noreply.github.com>
2025-04-29 17:34:43 +0200

    Update senttoServer.py
    Warnigs getting ignored + only scholl logger

--- BACKEND/rasberry/Steuerung/senttoServer.py
+++ BACKEND/rasberry/Steuerung/senttoServer.py
@@ -1,32 +1,34 @@
 import requests
+import json
+from datetime import datetime
+from DATA import DATA
+import urllib3
+
+# Suppress only the InsecureRequestWarning because the schhool server uses a self-signed certificate
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def send_json(data):
+    url = "https://10.10.30.35:5000/api/data"  #School server api url
+    urlcompanyserver = "https://resqdrive.ddns.net/server.php" #Company server api url
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": "Drive"#API key for the school server
+    }
+    print("Sende Daten:", data)#Debugging print
+    DATA.data.append(data)#append data to the list (Genereal DATA retun)
+    
+    try:
+        response = requests.post(url, json=data, headers=headers, verify=False)#post request to the school server
+        if response.status_code == 200:
+            try:
+                result = response.json()
+                return result
+            except json.JSONDecodeError:
+                return {"status": "error", "message": "Invalid JSON response from server"}
+        else:
+            return {"status": "error", "message": f"Server returned status code {response.status_code}"}#error on post request
+    except requests.exceptions.RequestException as e:
+         return {"status": "error", "message": str(e)}
 
 
-class Sender():
-    TARGET_RESPONSE_URL = ""
-    def __init__(self, url):
-        self.TARGET_RESPONSE_URL = url
-
-
-    def normsend(self, message):
-        try:
-            # Antwortdaten vorbereiten
-            response_data = {
-                "command": message,
-                "status": "processed",
-                "source": "Server"  # Zusätzliche Informationen
-            }
-            headers = {"Content-Type": "application/json"}
-
-            # POST-Anfrage an die Ziel-Website senden
-            response = requests.post(self.TARGET_RESPONSE_URL, json=response_data, headers=headers)
-
-            # Überprüfung der Antwort von der Ziel-Website
-            if response.status_code == 200:
-                print("Successfully sent response to target website.")
-
-            else:
-                # Debug Error Ruckgabe
-                print(f"Failed to send response. Status code: {response.status_code}")
-        except Exception as e:
-            print(f"Error sending response: {e}")
 
